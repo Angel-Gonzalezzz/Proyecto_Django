bcadec5648a23a0059e5ab3297e2aabfcc24b27d
Angel-Gonzalezzz <loveang36@gmail.com>
2026-06-03 00:36:02 -0600

    feat: :art: Proyecto_Django

--- pp/pp/urls.py
+++ pp/pp/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pp.views import login_view, register_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', login_view),
+    path('register/', register_view, name='register')
 ]
---
+++ pp/pp/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+        
+        if user is not None:
+            login(request, user)
+            return redirect('/admin/')
+        else:
+            messages.error(
+                request,
+                'Usuario o contraseña incorrectos'
+            )
+            
+    return render(request, 'login.html')
+
+def register_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        
+        if User.objects.filter(username=username).exists():
+            messages.error(
+                request,
+                'El usuario ya existe'
+            )
+            return redirect('/register/')
+            
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+        )
+        
+        user.save()
+        
+        messages.success(
+            request,
+            'Usuario registrado correctamente'
+        )
+        return redirect('/')
+        
+    return render(request, 'register.html')
